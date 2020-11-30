16effd181f3d5b054083744c9ba34984debed754
Seth Glenn <Sbgees@yahoo.com>
2020-11-30 17:39:35 -0600

    implementing .delete logic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,20 @@
-const router = require("express").Router();
+const express = require("express");
 const fs = require("fs");
 
+const router = express();
 let notesData = [];
 
 router
     // .route("/api/notes")
     .get("/api/notes", function(err, res) {
-        notesData = fs.readFileSync("./assets/db/db.json", "utf8");
+        notesData = fs.readFileSync("./db/db.json", "utf8");
         notesData = JSON.parse(notesData);
 
         res.json(notesData);
     });
 router
     .post("/api/notes", (req, res) => {
-        notesData = fs.readFileSync("./assets/db/db.json", "utf8");
+        notesData = fs.readFileSync("./db/db.json", "utf8");
         console.log(notesData);
 
         notesData = JSON.parse(notesData);
@@ -24,17 +25,25 @@ router
 
         notesData = JSON.stringify(notesData);
 
-        fs.writeFile("./assets/db/db.json", "utf8");
+        fs.writeFile("./db/db.json", "utf8");
 
         res.json(JSON.parse(notesData));
     });
 router
     .delete("/api/notes", (req, res) => {
-        notesData = fs.readFileSync("./assets/db/db.json", "utf8");
+        notesData = fs.readFileSync("./db/db.json", "utf8");
 
         notesData = JSON.parse(notesData);
 
-        
+        notesData = notesData.filter(function(note) {
+            return note.id != req.params.id;
+        })
+
+        notesData = JSON.stringify(notesData);
+
+        fs.writeFile("./db/db.json", "utf8");
+
+        res.send(JSON.parse(notesData));
 
     });
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,20 @@
 const path = require("path");
-const router = require("express").Router();
+const express = require("express");
+
+const router = express();
 
 router.get("/notes", (_req, res) => {
-    res.sendFile(path.join(__dirname, "assets/public/notes.html"));
+    res.sendFile(path.join(__dirname, "./assets/public/notes.html"));
 
 })
 
 router.get("*", (_req, res) => {
-    res.sendFile(path.join(__dirname, "assets/public/index.html"));
+    res.sendFile(path.join(__dirname, "./assets/public/index.html"));
 
 })
 
 router.get("/api/notes", (_req, res) => {
-    return res.sendFile(path.json(__dirname, "./assets/db/db.json"));
+    return res.sendFile(path.json(__dirname, "./db/db.json"))
 })
 
 module.exports = router;
