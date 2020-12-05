fdf8529532027bdaf658d328b74bafe8fe03510f
Seth Glenn <Sbgees@yahoo.com>
2020-12-05 15:12:22 -0600

    fixed routing issues/file pathes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,17 +4,13 @@ const express = require("express");
 const router = express();
 
 router.get("/notes", (_req, res) => {
-    res.sendFile(path.join(__dirname, "./assets/public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 
 })
 
 router.get("*", (_req, res) => {
-    res.sendFile(path.join(__dirname, "./assets/public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 
-})
-
-router.get("/api/notes", (_req, res) => {
-    return res.sendFile(path.json(__dirname, "../assets/db/db.json"));
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,78 @@
-// const express = require("express");
-// const routes = require("./routes/htmlRoutes");
-// const path = require("path");
+const express = require("express");
+const fs = require("fs");
+const router = require("express").Router();
+const htmlRoutes = require("./routes/htmlRoutes");
+const path = require("path");
+// const db = require("./db/db.json");
+const readAsync = require("util").promisify(fs.readFile);
 
-// const app = express();
 
-// const PORT = process.env.PORT || 8080;
 
-// app.use(express.urlencoded({ extended: true }));
 
-// app.use(express.json());
+const app = express();
+let notesData = [];
+// notesData = db;
 
-// app.use(express.static("./routes/assets/public"));
+// Server routes/middleware
+const PORT = process.env.PORT || 8080;
 
-// app.use(routes);
+app.use(express.urlencoded({ extended: true }));
 
-// app.listen(PORT, () => {
-//     console.log("App listening on PORT" + PORT);
-// })
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+
+router
+    .get("/notes", (req, res) => {
+        readAsync("./db/db.json", "utf8")
+        .then(notes => {
+            notesData = notesData.concat(JSON.parse(notes))
+        })
+        
+
+        res.json(notesData);
+       
+    });
+router
+    .post("/notes", (req, res) => {
+        notesData = fs.readFileSync("./db/db.json", "utf8");
+        console.log(notesData);
+
+        notesData = JSON.parse(notesData);
+
+        req.body.id = notesData.length;
+
+        notesData.push(req.body);
+
+        notesData = JSON.stringify(notesData);
+
+        fs.writeFile("./db/db.json", "utf8");
+
+        res.json(JSON.parse(notesData));
+    });
+router
+    .delete("/notes", (req, res) => {
+        notesData = fs.readFileSync("./db/db.json", "utf8");
+
+        notesData = JSON.parse(notesData);
+
+        notesData = notesData.filter(function(note) {
+            return note.id != req.params.id;
+        })
+
+        notesData = JSON.stringify(notesData);
+
+        fs.writeFile("./db/db.json", "utf8");
+
+        res.send(JSON.parse(notesData));
+
+    });    
+app.use("/api", router);
+
+app.use("/", htmlRoutes );
+
+app.listen(PORT, () => {
+    console.log("app listening on PORT" + PORT);
+})
